52cfabe6ad46adb224e0b0054317661db4b36d6f
davidmsf <davidmsf@gmail.com>
2018-09-08 16:42:03 +0200

    Customer and order entities

---
+++ PetShop.Core.Entity/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entity
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
--- PetShop.Core.Entity/Order.cs
+++ PetShop.Core.Entity/Order.cs
@@ -7,6 +7,8 @@ namespace PetShop.Core.Entity
     class Order
     {
         public int Id { get; set; }
-        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; } 
     }
 }
