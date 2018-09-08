e040f7d0e4261c050f924613c19022ab34ea9846
davidmsf <davidmsf@gmail.com>
2018-09-08 16:16:50 +0200

    removed unused old folder

--- PetShop.Core.Entity/Pet.cs
+++ PetShop.Core.Entity/Pet.cs
@@ -11,7 +11,7 @@ namespace PetShop.Core.Entity
         public string Type { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime SoldDate { get; set; }
-        public string color { get; set; }
+        public string Color { get; set; }
         public string PreviousOwner { get; set; }
         public double Price { get; set; }
         public Owner Owner { get; set; }
